62c151a1cae05989c3c67954e8ef90f388d494ca
Alex <alexrollins95@gmail.com>
2022-10-02 12:06:00 -0500

    more blog code

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -32,7 +32,10 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
-    from . import auth
+    # Blueprints
+    from . import auth, blog
     app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
